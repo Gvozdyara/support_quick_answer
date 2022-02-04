4a5d259f30539c82f5a85889edd44ed24df47508
Gvozdyara <fahrutdinov.n@mail.ru>
2022-02-04 15:02:21 +0300

    working in adding new section, no result

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ def add_existing_section_as_btn(current_window):
     with open('data.json', 'r') as file: # read support data
         help_data = json.load(file)
 
-        for key in help_data:
-            if
         k = 0 # var for row for buttons to enter to existing sections
         for i in help_data:
             i_btn = Button(current_window, text=i, width=20, command=lambda i=i: open_section(i))
@@ -20,18 +18,37 @@ def add_existing_section_as_btn(current_window):
 
 ## Add section  as buttons to the current section to the current window
 def add_section(window_name, section_name, entered_section_name):
+    global Current_directory
     with open('data.json', 'r') as file: # read support data
         help_data = json.load(file)
+
+    #remember the current path
+    new_directory = Current_directory + section_name + "/"
+    Current_directory = new_directory
+    sections = Current_directory.split("/")
+    sections.pop(-1)
+
+    for i in sections:
+        dict = {}
+        count = 0
+        if help_data.get(i, 'default') is not None:
+            dict[i] = help_data.get(i)
+
+
+
+
+    #add new element to the current section
+
+
     # add section(entry) value as a section to the data.json.load
-    current_section_dict = help_data[section_name]
-    current_section_dict = dict()
-    current_section_dict[entered_section_name] = "null" # main sections of the support data such as sensors' names
+
+     # main sections of the support data such as sensors' names
 
     #add the new section to the file data.json
     with open('data.json', 'w') as outfile:
-        json.dump(current_section_dict, outfile, indent=4)
+        json.dump(help_data, outfile, indent=4)
 
-    n = len(current_section_dict)
+    n = len(inner_list)
     new_name_btn = entered_section_name + str(n)
     new_name_btn = Button(window_name, text=entered_section_name, width=20,
                          command=lambda entered_section_name=entered_section_name:
@@ -59,14 +76,14 @@ def open_section(section_name):
     record_section_btn.grid(column=1, row=0)
 
 
+
+
 root = Tk()
 
 add_existing_section_as_btn(root)
 
-
-
-
-
+global Current_directory
+Current_directory = ""
 
 
 root.mainloop()
