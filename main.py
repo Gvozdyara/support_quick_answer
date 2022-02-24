61ec3c54d0f6c04f2d0ad4f1efff4b2f004ff9bf
Gvozdyara <fahrutdinov.n@mail.ru>
2022-02-24 20:26:41 +0300

    засунул внутреннюю таблицу внутрь ячейки, но опен секшн пока так и не сделал

--- main.py
+++ main.py
@@ -10,28 +10,42 @@ def dump_help_base():
     print([row for row in cur])
 
 
-def create_data_base(table_name):
-    db_file_name = table_name + ".db"
-    conn = sqlite3.connect(db_file_name)
+def create_data_base(file_name, table_name):
+    conn = sqlite3.connect(file_name)
     cur = conn.cursor()
-    q = '''CREATE TABLE IF NOT EXISTS "{}" (id INTEGER PRIMARY KEY AUTOINCREMENT, section_name TEXT)'''
-    cur.execute(q.format(table_name))
-    conn.commit()
-    print([row for row in cur.execute("SELECT * from {}".format(table_name))])
-    # debug
-    print("data base ", table_name, " created" )
-
+    create_table(cur, conn, table_name)
     return table_name
 
 
+def create_table (cur, conn, table_name):
+    q = '''CREATE TABLE IF NOT EXISTS "{}" (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                section_name TEXT, 
+                section_inner_lvl TEXT)'''
+    cur.execute(q.format(table_name))
+    conn.commit()
+    return table_name
+
 class section_btns:
-    def __init__(self, frame, section_id, text, click_cmnd):
+    def __init__(self, frame, section_id, text, click_cmnd, current_table):
         name = "btn_" + str(section_id)
-        self.name = Button(frame, text=text, command=lambda: click_cmnd(section_id))
+        self.name = Button(frame, text=text, command=lambda: click_cmnd(section_id, current_table))
         self.name.pack(fill=X)
         self.id = section_id
+        self.current_table = current_table
 
-
+    def create_inner_table_add_to_the_row(self, section_id, current_table):
+        # make the table name where to add new sections
+        table_name = "table_" + str(section_id)
+        conn = sqlite3.connect(Data_base_file)
+        cur = conn.cursor()
+        table = create_table(cur, conn, table_name)
+        #     надо добавить свеже созданую таблицу в таблицу выше уровнем в строку с section_id_var
+        q = """UPDATE '{}' 
+                SET section_inner_lvl = '{}'
+                WHERE rowid = '{}' """
+        cur.execute(q.format(current_table, table, section_id))
+        print("Inner table is nested")
 
 
 class new_section_entry:
@@ -45,13 +59,13 @@ class new_section_entry:
 
 
 
-
 def add_section(entry, current_table):
     global root, section_frame
     section_title = entry.get()
     section_id = add_section_to_db(section_title, current_table)
     #     for now we have unique section ID of the new section from the previous func
-    new_section_btn = section_btns(section_frame, section_id, section_title, open_section)
+    new_section_btn = section_btns(section_frame, section_id, section_title, open_section, current_table)
+    new_section_btn.create_inner_table_add_to_the_row(id, current_table)
 
 
 def add_section_to_db(section_name_var, current_table):
@@ -65,10 +79,12 @@ def add_section_to_db(section_name_var, current_table):
 
     cur.execute(q.format(current_table), (section_name_var,))
     conn.commit()
-    string = cur.execute('SELECT rowid, section_name FROM "{}"'.format(current_table))
-    print(string)
+    # возвращаем rowid добавленного раздела
+    q = """SELECT rowid FROM '{}'"""
+    section_id = cur.execute(q.format(current_table))
+    return section_id
 
-def open_section(section_id_var):
+def open_section(section_id_var, current_table):
     global section_frame, entry_frame
     # удаляем все кнопки с секциями из фрейма-кнопок для заполнения его новыми кнопками
     for widget in section_frame.winfo_children():
@@ -79,23 +95,15 @@ def open_section(section_id_var):
         widget.destroy()
     entry_frame.update()
     # отправляем команду на создание нового фрейма, нового Энтри  для добавления разделов внутрь открываемого
-    new_entry_name = "entry_" + str(section_id)
-    new_entry_name = new_section_entry(new_entry_name, entry_frame, section_id_var) #sectionid нужно добавить как rowid к вызову
-
-    # make the table name where to add new sections
-    table_name = "table_" + str(section_id_var)
-    conn = sqlite3.connect("helpbase.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS table_name (section_id INT PRIMARY KEY section_name TEXT)")
-    cur.commit()
-    print([row for row in cur.execute("SELECT * FROM table_name")])
-        
+    new_entry_name = "entry_" + str(section_id_var)
+    new_entry = new_section_entry(new_entry_name, entry_frame) # sectionid нужно добавить как rowid к вызову
 
 
 root = Tk()
 root.title("Quick respond")
 
-table_name = create_data_base("main")
+Data_base_file = "sections.db"
+table_name = create_data_base(Data_base_file, "main")
 
 section_frame = Frame(root, background="RED")
 section_frame.pack(side=BOTTOM, fill=X)
